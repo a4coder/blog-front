5ed24af16dcf4084da5c3da54b7b3b901ed65b98
Alireza Shahryari <eng.alirezashahryari@gmail.com>
2022-05-23 23:55:48 +0430

    home page 2

--- Assets/javascript/scripts.js
+++ Assets/javascript/scripts.js
@@ -9,11 +9,31 @@ $(function () {
 
   $(".close-menu").on("click", function () {
     $("#menu").css("right", "-250px");
-  })
+  });
 
-  $("")
+  $(".filter-active").on("click", function () {
+    $(".blog-box").css("display", "block");
+  });
 
-    // var portfolio = $("#blogs-container").isotope({
+  $("#programming").on("click", function () {
+    $(".programming").css("display", "block");
+    $(".skill").css("display", "none");
+    $(".technology").css("display", "none");
+  });
+
+  $("#skill").on("click", function () {
+    $(".skill").css("display", "block");
+    $(".programming").css("display", "none");
+    $(".technology").css("display", "none");
+  });
+
+  $("#technology").on("click", function () {
+    $(".technology").css("display", "block");
+    $(".programming").css("display", "none");
+    $(".skill").css("display", "none");
+  });
+
+  // var portfolio = $("#blogs-container").isotope({
   //   originLeft: false,
   // });
 
